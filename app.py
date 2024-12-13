30ae8e004c21ae52977675d50e0e9a0a398189cb
Febey695 <ratnaphoebe@gmail.com>
2024-12-13 16:16:19 +0530

    updating

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ def get_devices():
         return jsonify({"error": str(e)}), 500
 
 
-if_ _name__ == '__main__':
+if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
         port=2000,debug=True)
